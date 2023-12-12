3c5b9a769d68b8c0696ce1b1349872dbf51c05fe
Thiago Siqueira <siqueira.sths@gmail.com>
2023-12-12 16:34:15 -0300

    Update cash.c

--- pset1/cash/cash.c
+++ pset1/cash/cash.c
@@ -1,43 +1,84 @@
-#include <stdio.h>
 #include <cs50.h>
-#include <math.h>
+#include <stdio.h>
+
+int get_cents(void);
+int calculate_quarters(int cents);
+int calculate_dimes(int cents);
+int calculate_nickels(int cents);
+int calculate_pennies(int cents);
 
 int main(void)
 {
-    float dolars;
+    int cents = get_cents();
 
+    int quarters = calculate_quarters(cents);
+    cents = cents - quarters * 25;
+
+    int dimes = calculate_dimes(cents);
+    cents = cents - dimes * 10;
+
+    int nickels = calculate_nickels(cents);
+    cents = cents - nickels * 5;
+
+    int pennies = calculate_pennies(cents);
+    cents = cents - pennies * 1;
+
+    int coins = quarters + dimes + nickels + pennies;
+
+    printf("%i\n", coins);
+}
+
+int get_cents(void)
+{
+    int cents;
     do
     {
-        dolars = get_float("Change owed: $");
+        cents = get_float("Change owed: ");
     }
-    while (dolars < 0.0);
+    while (cents < 0.0);
+    return cents;
+}
 
-    int cents = round(dolars * 100);
-
-    int c25 = 0, c10 = 0, c5 = 0, c1 = 0;
-
+int calculate_quarters(int cents)
+{
+    int c25 = 0;
     while (cents >= 25)
     {
         cents -= 25;
         c25++;
     }
+    return c25;
+}
+
+int calculate_dimes(int cents)
+{
+    int c10 = 0;
     while (cents >= 10)
     {
         cents -= 10;
         c10++;
     }
+    return c10;
+}
+
+int calculate_nickels(int cents)
+{
+    int c5 = 0;
     while (cents >= 5)
     {
         cents -= 5;
         c5++;
     }
+    return c5;
+}
+
+int calculate_pennies(int cents)
+{
+    int c1 = 0;
     while (cents >= 1)
     {
         cents -= 1;
         c1++;
     }
-    printf("Quarters: %i\n", c25);
-    printf("Dimes: %i\n", c10);
-    printf("Nickles: %i\n", c5);
-    printf("Pennies: %i\n", c1);
+    return c1;
 }
