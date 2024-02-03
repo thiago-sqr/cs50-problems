39dd08704a6d76f50ac2722a1ddc3d0529bcf28d
Thiago Siqueira <siqueira.sths@gmail.com>
2024-02-02 22:08:11 -0300

    Create movies

---
+++ pset7/movies/4.sql
@@ -0,0 +1,8 @@
+-- write a SQL query to determine the number of movies with an IMDb rating of 10.0.
+SELECT COUNT(*)
+FROM movies
+WHERE id IN (
+    SELECT movie_id
+    FROM ratings
+    WHERE rating = 10.0
+);
